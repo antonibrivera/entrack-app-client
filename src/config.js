de4c1ec0526c470a72cfb5654e59bd63a26ca8f6
Joseph Rivera <josephr98@gmail.com>
2020-06-02 12:44:49 -0400

    quicksave: configuring './src/config.js' and environment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8080',
-  TOKEN_KEY: 'entrack-client-auth-token',
+  API_ENDPOINT: process.env.API_ENDPOINT || 'http://localhost:8080',
+  TOKEN_KEY: process.env.TOKEN_KEY || 'entrack-client-auth-token',
 }
